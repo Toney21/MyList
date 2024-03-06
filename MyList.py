04952869c7f471f6471c1747632ce3030922f88d
unknown <you@example.com>
2024-03-06 17:23:59 +0300

    first commit

---
+++ MyList.py
@@ -0,0 +1,8 @@
+my_list = []
+my_list.extend([10, 20, 30, 40])
+my_list.insert(1,15)
+my_list.extend([50,60,70])
+my_list.pop()
+my_list.sort()
+index_30 = my_list.index(30)
+print("The index of 30 in my list:", index_30)
