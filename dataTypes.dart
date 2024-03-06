86c250084901da19b5a3628839663f0200605fc4
unknown <you@example.com>
2024-03-06 17:50:14 +0300

    first commit

---
+++ dataTypes.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Integer data type example
+  int age = 20;
+  print('Age: $age'); // Output: Age: 25
+
+  // Double data type example
+  double weight = 65.5;
+  print('Weight: $weight kg'); // Output: Weight: 65.5 kg
+
+  // String data type example
+  String name = 'John Doe';
+  print('Name: $name'); // Output: Name: John Doe
+
+  // List data type example
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('Numbers: $numbers'); // Output: Numbers: [1, 2, 3, 4, 5]
+
+  // Map data type example
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'isStudent': false,
+  };
+  print('Person: $person'); // Output: Person: {name: Alice, age: 30, isStudent: false}
+}
